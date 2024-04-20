f97d1fcb432a73557d82e0b6f86d0c3216e58afe
mansijaiswal <96405456+mansijaiswal12@users.noreply.github.com>
2024-04-20 13:42:14 +0530

    about page updated

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TechStack from './TechStack'
+
+const About = () => {
+  return (
+    <div className='text-slate-50 flex flex-col items-center flex-wrap'>
+
+      <TechStack />
+      
+    </div>
+  )
+}
+
+export default About
