1e415d1978c409054a6ba33fe2fde5da0c13b942
mansijaiswal <96405456+mansijaiswal12@users.noreply.github.com>
2024-04-24 22:24:21 +0530

    particle jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Navbar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
 
-// import Resume from './components/Resume'
+import Resume from './components/Resume'
 import Project from './components/Project'
-// import Footer from './components/Footer'
+import Footer from './components/Footer'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import './App.css'
 const App = () => {
@@ -17,12 +17,12 @@ const App = () => {
         <Route path='/' element={<Home/>}/>
         <Route path='/About' element={<About />}/>
         <Route path='/Projects' element={<Project/>}/>
-        {/* <Route path='/Resume' element={<Resume/>}/> */}
-        {/* <Route path='/Footer' element={<Footer/>}/> */}
+        <Route path='/Resume' element={<Resume/>}/>
+        <Route path='/Footer' element={<Footer/>}/>
 
      
       </Routes>
-      {/* <Footer /> */}
+      <Footer />
      
     </BrowserRouter>
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <footer style={{color:'--text-color'}} className='bg-gray-400 text-lg' >
+        <h6><span className='flex  gap-2 mt-4 justify-start'>Designed and developed by Mansi Jaiswal</span>
+          <span className='flex justify-center gap-2'>Copyright@2024 MJ</span></h6>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
